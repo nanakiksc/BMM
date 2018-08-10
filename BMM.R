03cbdee7dc89a99a842009109861a7ab74504196
nanakiksc <polcusco@gmail.com>
2018-08-10 13:40:36 +0200

    Added input scaling. Found bugs.

--- BMM.R
+++ BMM.R
@@ -1,8 +1,10 @@
 BMM <- function(x, k, n.iter = 1e3) {
+    x <- sort(x)
+    x <- (x - min(x) + .Machine$double.eps) / (max(x) - min(x) + 2 * .Machine$double.eps)
     y <- rep(1:k, each = length(x) / k)
     y <- c(y, rep(k, length(x) - length(y)))
     params <- data.frame(a = rep(0, k), b = rep(0, k))
-    probs <- matrix(nrow = k, ncol = length(x))
+    probs <- matrix(nrow = length(x), ncol = k)
 
     for (i in 1:n.iter) {
         # E step.
@@ -14,9 +16,9 @@ BMM <- function(x, k, n.iter = 1e3) {
 
         # M step.
         prior <- table(y) / length(y)
-        for (p in 1:nrow(params)) probs[p, ] <- dbeta(x, params$a[p], params$b[p]) * prior[p]
-        probs <- t(apply(probs, 1, '/', colSums(probs)))
-        y <- apply(probs, 2, which.max)
+        for (p in 1:nrow(params)) probs[, p] <- dbeta(x, params$a[p], params$b[p]) * prior[p]
+        probs <- apply(probs, 2, '/', rowSums(probs))
+        y <- max.col(y)
     }
     list(y = y, params = params, prior = prior)
 }
